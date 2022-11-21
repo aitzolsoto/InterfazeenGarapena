66d343ac7a9c1ef7012157d68612d285d8587b65
aitzolsoto <56816883+aitzolsoto@users.noreply.github.com>
2022-11-21 12:11:40 +0100

    Adibidea

---
+++ ASP/Adibidea/Adibidea/Adibidea/Models/Kontaktua.cs
@@ -0,0 +1,22 @@
+﻿namespace Adibidea.Models
+{
+    public class Kontaktua
+    {
+        public string nan { get; set; }
+        public string izena { get; set; }
+        public string abizena { get; set; }
+        public virtual string emaila { get; set; }
+        public string izenOsoa { get { return izena + "" + abizena; } }
+        public Kontaktua(string nan, string izena, string abizena)
+        {
+            this.nan = nan;
+            this.izena = izena;
+            this.abizena = abizena;
+        }
+
+        public virtual string Gorde()
+        {
+            return "Kontaktua ondo gorde da";
+        }
+    }
+}
